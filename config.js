a02ae59d4732bd4788bc05f92c7c8e8395faa072
bklynate <nathanielcfa@gmail.com>
2017-03-05 14:21:25 -0500

    blog ready

--- config.js
+++ config.js
@@ -11,19 +11,21 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: process.env.BLOG_URL,
         mail: {},
         database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
+          client: 'postgres',
+          connection: {
+            host: process.env.POSTGRES_HOST,
+            database: process.env.POSTGRES_DATABASE,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            port: '5432'
+          }
         },
-
         server: {
-            host: '127.0.0.1',
-            port: '2368'
+            host: '0.0.0.0',
+            port: process.env.PORT
         }
     },
 
